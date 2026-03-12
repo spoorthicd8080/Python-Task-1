abb1cb693896286dccbd2c6f4027fc7609a9117b
spoorthicd8080 <spoorthicd8080@gmail.com>
2026-03-12 19:38:49 +0530

    Update Assignment.py

---
+++ Assignment.py
@@ -0,0 +1,98 @@
+# Task 1 Print formatting 
+
+print("Hello", "World", "Welcome", end=" ")
+print("Python")
+print("Laptop", "Mouse", "Keyboard", sep=" | ")
+
+#Task 2 Variables
+
+name = "Ravi"
+age = 22
+city = "Chennai"
+print(name, age, city, sep="-")
+
+#Task 3 Multiple Assignment 
+
+name, age, student = "Meena", 20, True
+print(name)
+print(age)
+print(student)
+
+#Task 4 Indexing
+
+word = "Python"
+
+print(word[0])
+print(word[2])
+print(word[-1])
+
+#Task 5 Arithmetic Operators
+
+print("1.", 25 + 10)
+print("2.", 50 - 20)
+print("3.", 8 * 5)
+print("4.", 100 / 10)
+print("5.", 10 % 3)
+print("6.", 2 ** 4)
+print("7.", 20 // 3)
+
+#Task 6 Bodmas Expression
+
+print(3 + 2 * 5 **2)
+
+#Task 7 Assignment Operator
+
+num = 50
+num += 25
+
+print(num) 
+
+num = 100
+num/=10
+
+print(num)
+
+#Task 8 Comparison Operations
+
+print(10 > 5)
+print(20 < 15)
+print(5 == 5)
+print(10 != 8)
+print(7 >= 7)
+print(6 <= 2)
+
+#Task 9 String Comparison
+
+a = "apple"
+b = "Apple"
+
+print(a == b)
+
+#Task 10 Logical Operators
+
+print(10 > 5 and 5 == 5)
+print(5 > 10 or 10 == 10)
+print(not(5 > 2))
+
+#Task 11 Membership Operator
+
+numbers = [10, 20, 30, 40, 50]
+
+print(20 in numbers)
+print(60 in numbers)
+print(30 not in numbers)
+
+#Task 12 Swap Variables
+
+a = 10
+b = 20
+
+print("a =", a)
+print("b =", b)
+
+#Task 13 Bitwise XOR
+
+a = 6
+b = 3
+
+print(a ** b)
